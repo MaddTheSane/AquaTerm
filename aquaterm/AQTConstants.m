187629d76963823802debfdf0fce513402b43e95
C.W. Betts <computers57@hotmail.com>
2016-08-15 16:10:23 -0600

    Actually export AQTButtLineCapStyle and friends for binary compatibility.

--- aquaterm/AQTConstants.m
+++ aquaterm/AQTConstants.m
@@ -1,5 +1,20 @@
 #include <stdint.h>
 
+// The declarations here are only for binary compatibility!
+
+extern const int32_t AQTButtLineCapStyle;
+extern const int32_t AQTRoundLineCapStyle;
+extern const int32_t AQTSquareLineCapStyle;
+
+extern const int32_t AQTAlignLeft;
+extern const int32_t AQTAlignCenter;
+extern const int32_t AQTAlignRight;
+extern const int32_t AQTAlignMiddle;
+extern const int32_t AQTAlignBaseline;
+extern const int32_t AQTAlignBottom;
+extern const int32_t AQTAlignTop;
+
+
 const int32_t AQTButtLineCapStyle = 0;
 const int32_t AQTRoundLineCapStyle = 1;
 const int32_t AQTSquareLineCapStyle = 2;

2d52053bb612a9ce06ea171a96995129211972f2
C.W. Betts <computers57@hotmail.com>
2025-07-11 04:00:09 -0600

    Minor pokes to AquaTermCSwiftDemo.

--- aquaterm/AquaTermCSwiftDemo/main.swift
+++ aquaterm/AquaTermCSwiftDemo/main.swift
@@ -8,8 +8,6 @@
 
 import Swift
 import AquaTerm.AquaTerm
-import Darwin.sys.stdio
-import Darwin.C.math
 
 private func internalMain() {
    let rgbImage: [UInt8] = [255, 0, 0,
@@ -50,8 +48,8 @@ private func internalMain() {
    // Colormap
    aqtAddLabel("Custom colormap (8 out of 256)", 30, 390, 0.0, [])
    // Display the colormap, but first create a background for the white box...
-   aqtSetColor(0.8, 0.8, 0.8);
-   aqtAddFilledRect(28, 348, 24, 24);
+   aqtSetColor(0.8, 0.8, 0.8)
+   aqtAddFilledRect(28, 348, 24, 24)
    for i in Int32(0) ..< 8 {
       aqtTakeColorFromColormapEntry(i)
       aqtAddFilledRect(30+Float(i)*30, 350, 20, 20)
@@ -61,8 +59,8 @@ private func internalMain() {
    }
    // Continuos colors
    aqtTakeColorFromColormapEntry(1)
-   aqtAddLabel(#""Any color you like""#, 320, 390, 0.0, []);
-   aqtSetLinewidth(1.0);
+   aqtAddLabel(#""Any color you like""#, 320, 390, 0.0, [])
+   aqtSetLinewidth(1.0)
    for i in stride(from: Float(0), to: 256, by: 1) {
       let f = i/255.0
       aqtSetColor(1.0, f, f/2.0)
@@ -74,7 +72,7 @@ private func internalMain() {
    }
 
    // Lines
-   aqtTakeColorFromColormapEntry(1);
+   aqtTakeColorFromColormapEntry(1)
    for f in stride(from: Float(1), to: 13, by: 2) {
       let lw = f / 2.0
       aqtSetLinewidth(lw)
@@ -119,7 +117,7 @@ private func internalMain() {
    aqtAddLineTo(150.5, 130.5)
 
    // line joins
-   aqtTakeColorFromColormapEntry(1);
+   aqtTakeColorFromColormapEntry(1)
    aqtAddLabel("Line joins:", 40, 90, 0.0, [])
    aqtSetLinewidth(11.0)
    aqtSetLineCapStyle(.butt)
@@ -132,31 +130,31 @@ private func internalMain() {
    aqtAddLineTo(75, 70)
    aqtAddLineTo(110, 50)
 
-   aqtSetLinewidth(11.0);
-   aqtTakeColorFromColormapEntry(1);
-   aqtMoveTo(130, 50);
-   aqtAddLineTo(150, 70);
-   aqtAddLineTo(170, 50);
-   aqtSetLinewidth(1.0);
-   aqtTakeColorFromColormapEntry(6);
-   aqtMoveTo(130, 50);
-   aqtAddLineTo(150, 70);
-   aqtAddLineTo(170, 50);
+   aqtSetLinewidth(11.0)
+   aqtTakeColorFromColormapEntry(1)
+   aqtMoveTo(130, 50)
+   aqtAddLineTo(150, 70)
+   aqtAddLineTo(170, 50)
+   aqtSetLinewidth(1.0)
+   aqtTakeColorFromColormapEntry(6)
+   aqtMoveTo(130, 50)
+   aqtAddLineTo(150, 70)
+   aqtAddLineTo(170, 50)
 
-   aqtSetLinewidth(11.0);
-   aqtTakeColorFromColormapEntry(1);
-   aqtSetLineCapStyle(AQTLineCapStyle.butt);
-   aqtMoveTo(190, 50);
-   aqtAddLineTo(200, 70);
-   aqtAddLineTo(210, 50);
-   aqtSetLinewidth(1.0);
-   aqtTakeColorFromColormapEntry(6);
-   aqtMoveTo(190, 50);
-   aqtAddLineTo(200, 70);
-   aqtAddLineTo(210, 50);
+   aqtSetLinewidth(11.0)
+   aqtTakeColorFromColormapEntry(1)
+   aqtSetLineCapStyle(AQTLineCapStyle.butt)
+   aqtMoveTo(190, 50)
+   aqtAddLineTo(200, 70)
+   aqtAddLineTo(210, 50)
+   aqtSetLinewidth(1.0)
+   aqtTakeColorFromColormapEntry(6)
+   aqtMoveTo(190, 50)
+   aqtAddLineTo(200, 70)
+   aqtAddLineTo(210, 50)
 
    // Polygons
-   aqtTakeColorFromColormapEntry(1);
+   aqtTakeColorFromColormapEntry(1)
    aqtAddLabel("Polygons", 320, 290, 0.0, [])
    for i in 0 ..< 4 {
       let radians = Float(i) * pi/2.0
@@ -195,8 +193,8 @@ private func internalMain() {
       xPtr.append(460.0+r*cos(radians))
       yPtr.append(255.0+r*sin(radians))
    }
-   aqtTakeColorFromColormapEntry(4);
-   aqtAddPolygon(&xPtr, &yPtr, 8);
+   aqtTakeColorFromColormapEntry(4)
+   aqtAddPolygon(&xPtr, &yPtr, 8)
 
    xPtr.removeAll(keepingCapacity: true)
    yPtr.removeAll(keepingCapacity: true)
@@ -208,45 +206,45 @@ private func internalMain() {
       xPtr.append(520.0+r*cos(radians))
       yPtr.append(255.0+r*sin(radians))
    }
-   aqtTakeColorFromColormapEntry(5);
-   aqtAddPolygon(&xPtr, &yPtr, 32);
+   aqtTakeColorFromColormapEntry(5)
+   aqtAddPolygon(&xPtr, &yPtr, 32)
 
    xPtr.removeAll(keepingCapacity: false)
    yPtr.removeAll(keepingCapacity: false)
    
    
    // Images
-   aqtTakeColorFromColormapEntry(1);
-   aqtAddLabel("Images", 320, 220, 0.0, []);
-   aqtAddImageWithBitmap(rgbImage, 2, 2, 328, 200, 4, 4);
-   aqtAddLabel("bits", 330, 180, 0.0, [.center]);
-   aqtAddImageWithBitmap(rgbImage, 2,2, 360, 190, 40, 15);
-   aqtAddLabel("fit bounds", 380, 180, 0.0, [.center]);
-   aqtSetImageTransform(9.23880, 3.82683, -3.82683, 9.23880, 494.6, 186.9);
-   aqtAddTransformedImageWithBitmap(rgbImage, 2,2, 0.0, 0.0, 600.0, 400.0);
-   aqtAddLabel("scale, rotate & translate", 500, 180, 0.0, .center);
-   aqtResetImageTransform();
+   aqtTakeColorFromColormapEntry(1)
+   aqtAddLabel("Images", 320, 220, 0.0, [])
+   aqtAddImageWithBitmap(rgbImage, 2, 2, 328, 200, 4, 4)
+   aqtAddLabel("bits", 330, 180, 0.0, [.center])
+   aqtAddImageWithBitmap(rgbImage, 2, 2, 360, 190, 40, 15)
+   aqtAddLabel("fit bounds", 380, 180, 0.0, [.center])
+   aqtSetImageTransform(9.23880, 3.82683, -3.82683, 9.23880, 494.6, 186.9)
+   aqtAddTransformedImageWithBitmap(rgbImage, 2,2, 0.0, 0.0, 600.0, 400.0)
+   aqtAddLabel("scale, rotate & translate", 500, 180, 0.0, .center)
+   aqtResetImageTransform()
    
     // Text
-    aqtTakeColorFromColormapEntry(1);
-    aqtSetFontname("Times-Roman");
-    aqtSetFontsize(16.0);
-    aqtAddLabel("Times-Roman 16pt", 320, 150, 0.0, []);
-    aqtTakeColorFromColormapEntry(2);
-    aqtSetFontname("Times-Italic");
-    aqtSetFontsize(16.0);
-    aqtAddLabel("Times-Italic 16pt", 320, 130, 0.0, []);
-    aqtTakeColorFromColormapEntry(4);
-    aqtSetFontname("Zapfino");
-    aqtSetFontsize(12.0);
-    aqtAddLabel("Zapfino 12pt", 320, 104, 0.0, []);
+    aqtTakeColorFromColormapEntry(1)
+    aqtSetFontname("Times-Roman")
+    aqtSetFontsize(16.0)
+    aqtAddLabel("Times-Roman 16pt", 320, 150, 0.0, [])
+    aqtTakeColorFromColormapEntry(2)
+    aqtSetFontname("Times-Italic")
+    aqtSetFontsize(16.0)
+    aqtAddLabel("Times-Italic 16pt", 320, 130, 0.0, [])
+    aqtTakeColorFromColormapEntry(4)
+    aqtSetFontname("Zapfino")
+    aqtSetFontsize(12.0)
+    aqtAddLabel("Zapfino 12pt", 320, 104, 0.0, [])
 
-   aqtTakeColorFromColormapEntry(2);
-   aqtSetLinewidth(0.5);
-   aqtMoveTo(510.5, 160);
-   aqtAddLineTo(510.5, 100);
-   let x: Float = 540.5;
-   let y: Float = 75.5;
+   aqtTakeColorFromColormapEntry(2)
+   aqtSetLinewidth(0.5)
+   aqtMoveTo(510.5, 160)
+   aqtAddLineTo(510.5, 100)
+   let x: Float = 540.5
+   let y: Float = 75.5
    aqtMoveTo(x+5, y)
    aqtAddLineTo(x-5, y)
    aqtMoveTo(x, y+5)
